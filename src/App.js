d0414e21243217a70e4f6051e744bf51535bc80d
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-08-27 16:28:03 -0500

    Creando estados

--- src/App.js
+++ src/App.js
@@ -4,23 +4,38 @@ import './App.css';
 
 class App extends Component{
 
-  getBoxOne(number){
-    return(
-      <div>
-        <h2>{"Hola Mundo yo soy la cajita # " + number}</h2>
-      </div>
-    );
-  }
+constructor(){
+  super();
+
+this.state = {
+  //declaramos las variables de estado q van a cambiar su valor continuamente
+  text:"Hola Mundo",
+  text2: "Estoy aprendiendo react.js"
+};
+
+//onClick, porq es6 debemos hacer un bind
+this.onClick = this.onClick.bind(this);
+
+
+}
+
+onClick(e){
+  this.setState({text2: "ups el estado ha cambiado"});
+}
 
   render(){
     return (
       <div>
-        {this.getBoxOne(1)}
-        {this.getBoxOne(2)}
+        <h2 onClick={this.onClick}>{this.state.text}</h2>
+        <h4>{this.state.text2}</h4>
       </div>
     );
   }
 }
+
+//si hago un log aca para acceder a los state
+//estamos fuera del scope de donde vive la variable de estado
+
 
  //Exportamos este Component para q sea usado en otro lado
 export default App;
